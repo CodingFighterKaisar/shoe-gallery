cb9439c9721e23fa79c749715b50a25eb1c1e54c
Mdkaisarhamid <kaisar.hamid.business@gmail.com>
2023-10-23 21:02:01 +0600

    project sticky navbar has been created

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -6,7 +6,7 @@ import {
   Typography,
 } from "@material-tailwind/react";
 import React, { useEffect, useState } from "react";
-
+import logo from "../../assets/logo/site-logo.png";
 const Header = () => {
   const [openNav, setOpenNav] = useState(false);
   useEffect(() => {
@@ -63,12 +63,11 @@ const Header = () => {
     <div>
       <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
         <div className="flex items-center justify-between text-blue-gray-900">
-          <Typography
-            as="a"
-            href="#"
-            className="mr-4 cursor-pointer py-1.5 font-medium"
-          >
-            Material Tailwind
+          <Typography className="text-lg flex justify-center items-center">
+            <img src={logo} className="w-12" alt="" />
+            <span className="text-md text-blue-800 font-bold">
+              Shoes-Gallery
+            </span>
           </Typography>
           <div className="flex items-center gap-4">
             <div className="mr-4 hidden lg:block">{navList}</div>
